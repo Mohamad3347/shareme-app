18685ecffcb4f479c3d018f0d74185c0ef0f4297
mlaies <Mohamad.mlaies3347@gmail.com>
2022-11-30 11:52:49 +0100

    upload the project

---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: "save",
+  title: "Save",
+  type: "document",
+  fields: [
+    {
+      name: "postedBY",
+      title: "PostedBY",
+      type: "postedBy",
+    },
+    {
+      name: "userId",
+      title: "UserId",
+      type: "string",
+    },
+  ],
+};
